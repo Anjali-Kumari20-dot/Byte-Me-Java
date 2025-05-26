7132b9c32632f3e019a6bcf74c8dbd26d6f62e74
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-05-26 14:09:34 +0530

    Input

---
+++ variables_and_datatypes/Input_in_java.java
@@ -0,0 +1,20 @@
+package variables_and_datatypes;
+
+import java.util.Scanner;
+
+public class Input_in_java {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // String input2 = sc.nextLine();
+        // System.out.println(input2);
+
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+        float price = sc.nextFloat();
+        System.out.println(price);
+    }
+}
