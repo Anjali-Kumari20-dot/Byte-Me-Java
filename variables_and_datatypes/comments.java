fb711a39b3fb2eead3813b118d11d094bcd864dc
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-05-26 13:08:25 +0530

    Comments in java

---
+++ variables_and_datatypes/comments.java
@@ -0,0 +1,11 @@
+package variables_and_datatypes;
+
+public class comments {
+    public static void main(String[] args) {
+        // code to calculate sum
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
