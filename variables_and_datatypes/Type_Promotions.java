e95b3a3b11617dd4fac71d044aeea12bb99f8830
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-05-27 11:24:56 +0530

    Type promotion

---
+++ variables_and_datatypes/Type_Promotions.java
@@ -0,0 +1,9 @@
+package variables_and_datatypes;
+
+public class Type_Promotions {
+    public static void main(String[] args) {
+        byte b = 5;
+        b = (byte)(b * 2);
+        System.out.println(b);
+    }
+}
