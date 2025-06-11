f5a9febb0a37672c7627496c8b7a6213f3f1127e
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-06-11 07:19:57 +0530

    Insertion sort and inbuilt sort

--- BasicSorting/InbuiltSort.java
+++ BasicSorting/InbuiltSort.java
@@ -1,7 +1,23 @@
 package BasicSorting;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class InbuiltSort {
+    public static void printArray(Integer[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        
+        Integer arr[] = { 5, 4, 3, 1, 2 };
+        // Sorting the first three elements (indices 0 to 2) in natural order
+        Arrays.sort(arr, 0, 3);
+        printArray(arr);
+        // Sorting the entire array in descending order
+        Arrays.sort(arr, Collections.reverseOrder());
+        printArray(arr);
+
     }
 }
--- BasicSorting/InsertionSort.java
+++ BasicSorting/InsertionSort.java
@@ -1,7 +1,27 @@
 package BasicSorting;
 
 public class InsertionSort {
+    public static void insertionSort(int[] nums){
+        for(int i = 1; i < nums.length; i++){
+            int curr = nums[i];
+            int prev = i - 1;
+            // finding the correct position
+            while(prev >= 0 && nums[prev] > curr){
+                nums[prev + 1] = nums[prev];
+                prev--;
+            }
+            // insertion
+            nums[prev + 1] = curr;
+        }
+    }
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
     public static void main(String[] args) {
-        
+        int arr[] = { 5, 4, 3, 1, 2 };
+        insertionSort(arr);
+        printArray(arr);
     }
 }
