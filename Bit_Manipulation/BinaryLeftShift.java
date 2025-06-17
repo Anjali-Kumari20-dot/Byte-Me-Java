91db095f556b4360d8b7e61196dbd102a93eaa17
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-06-17 11:47:43 +0530

    Binary << Shift

---
+++ Bit_Manipulation/BinaryLeftShift.java
@@ -0,0 +1,9 @@
+package Bit_Manipulation;
+
+import java.util.*;
+
+public class BinaryLeftShift {
+    public static void main(String[] args) {
+        System.out.println("5<<2 : " + (5 << 2));
+    }
+}
