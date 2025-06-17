77d89c465fce1f249750a34199b65e27833f9024
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-06-17 11:39:08 +0530

    Binary 1's complement

---
+++ Bit_Manipulation/BinaryOnesComplement.java
@@ -0,0 +1,8 @@
+package Bit_Manipulation;
+
+public class BinaryOnesComplement {
+    public static void main(String[] args) {
+        System.out.println("~5 : " + (~5) );
+        System.out.println("~0 : " + (~0) );
+    }
+}
