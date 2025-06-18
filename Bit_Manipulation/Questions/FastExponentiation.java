6e9486dfa908f74823bdaa36436beac9a0cbd1e6
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-06-18 10:43:19 +0530

    Fast exponent

---
+++ Bit_Manipulation/Questions/FastExponentiation.java
@@ -0,0 +1,19 @@
+package Bit_Manipulation.Questions;
+
+public class FastExponentiation {
+    public static int power(int a, int n) {
+        int ans = 1;
+        while (n > 0) {
+            if((n & 1) != 0){ // check LSB
+                ans = ans * a;
+            }
+            a = a * a;
+            n >>= 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(5, 3));
+    }
+}
