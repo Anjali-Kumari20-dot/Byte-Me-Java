044d634250e8072d8e4d222123f8b85a103ca79f
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-07-08 08:07:55 +0530

    tilling problem

--- Recursion/XRaiseToPowerN.java
+++ Recursion/XRaiseToPowerN.java
@@ -6,9 +6,23 @@ public class XRaiseToPowerN {
         if(n == 1) return x;
         return x * xPowerN(x, n - 1);
     }
+    public static int optimisedPower(int x, int n){
+        if(n == 0){
+            return 1;
+        }
+        int halfPower = optimisedPower(x, n/2);
+        int halfPowerSq = halfPower * halfPower;
+
+        // n is odd
+        if(n % 2 != 0){
+            halfPowerSq = x * halfPowerSq;
+        }
+        return halfPowerSq;
+    }
     public static void main(String[] args) {
         int x = 8;
         int n = 3;
         System.out.println(xPowerN(x, n));
+        System.out.println(optimisedPower(x, n));
     }
 }
