d3b5a05c9e7282f6c79710b7a4f782621e8013a4
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-07-13 20:32:01 +0530

    Find subsets in In Array

--- Backtracking/FindSubsets.java
+++ Backtracking/FindSubsets.java
@@ -1,7 +1,9 @@
 package Backtracking;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FindSubsets {
-    public static void findSubsets (String str, int i, String ans){
+    public static void findSubsetsInStrings (String str, int i, String ans){
         if(i == str.length()){
             if(ans.length() == 0){
                 System.out.println("null");
@@ -10,10 +12,35 @@ public class FindSubsets {
             }
             return;
         }
-        findSubsets(str, i + 1, ans+str.charAt(i));
-        findSubsets(str, i + 1, ans);
+        findSubsetsInStrings(str, i + 1, ans+str.charAt(i));
+        findSubsetsInStrings(str, i + 1, ans);
+    }
+    public static void printArray(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+    public static void findSubsetsInArray(int[] arr, int i, List<Integer> ans){
+        if(i == arr.length){
+            if(ans.isEmpty()){
+                System.out.println("null");
+            }else{
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        ans.add(arr[i]);
+        findSubsetsInArray(arr, i+1, ans);
+
+        ans.remove(ans.size() - 1);
+        findSubsetsInArray(arr, i+1, ans);
+
     }
     public static void main(String[] args) {
-        findSubsets("abc", 0, "");
+        findSubsetsInStrings("abc", 0, "");
+        findSubsetsInArray(new int[]{1, 2, 3}, 0, new ArrayList<>());
     }
 }
+// Leetcode problem - 78
