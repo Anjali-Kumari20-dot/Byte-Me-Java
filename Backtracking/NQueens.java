bb738de4d9d3008856f7d6918118dc71285039b0
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-07-14 16:13:01 +0530

    count all possible ways and print one solution

--- Backtracking/NQueens.java
+++ Backtracking/NQueens.java
@@ -31,6 +31,7 @@ public class NQueens {
         // base
         if (row == board.length) {
             printBoard(board);
+            count++;
             return;
         }
 
@@ -44,18 +45,42 @@ public class NQueens {
         }
     }
 
+    public static boolean nQueensOneSol(char board[][], int row) {
+        // base
+        if (row == board.length) {
+            count++;
+            return true;
+        }
+
+        // columns loop
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                if (nQueensOneSol(board, row + 1)) {
+                    return true;
+                }
+                board[row][j] = 'x'; // backtracking step
+            }
+        }
+
+        return false;
+
+    }
+
     public static void printBoard(char[][] board) {
+        System.out.println("------- Chess Board -------");
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println();
     }
 
+    static int count = 0;
+
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5;
         char board[][] = new char[n][n];
         // initialise
         for (int i = 0; i < n; i++) {
@@ -63,6 +88,13 @@ public class NQueens {
                 board[i][j] = 'x';
             }
         }
-        nQueens(board, 0);
+        // nQueens(board, 0);
+        if(nQueensOneSol(board, 0)){
+            System.out.println("Solution is possible");
+            printBoard(board);
+        } else {
+            System.out.println("Solution is not possible");
+        }
+        System.out.println("Total ways to solve n queens : " + count);
     }
 }
