460be4d1fefbe3845d9a7abb3c594670f33a2aa7
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-07-24 15:18:09 +0530

    Container with Most Water

--- ARRAYLIST/multidimensional.java
+++ ARRAYLIST/multidimensional.java
@@ -20,5 +20,14 @@ public class multidimensional {
         mainList.add(list3);
 
         System.out.println(mainList);
+
+        // nested loops
+        for(int i = 0; i < mainList.size(); i++){
+            ArrayList<Integer> currList = mainList.get(i);
+            for(int j = 0; j < currList.size(); j++){
+                System.out.print(currList.get(j) + " ");
+            }
+            System.out.println();
+        }
     }
 }
