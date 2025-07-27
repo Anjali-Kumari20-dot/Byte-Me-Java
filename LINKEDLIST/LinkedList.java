a79b0f6f3e7cfd350161eee83c9e9cdbb5d1d449
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-07-27 16:07:10 +0530

    Add First and Add Last method added

--- LINKEDLIST/LinkedList.java
+++ LINKEDLIST/LinkedList.java
@@ -9,13 +9,46 @@ public class LinkedList {
             this.data = data;
             this.next = null;
         }
-
     }
 
     public static Node head;
     public static Node tail;
 
+    // Add first - O(1)
+    public void addFirst(int data){
+        // Step1 = create new Node
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        // Step2 - newNode next =head;
+        newNode.next = head;
+
+        // step3 : head = newNode
+        head = newNode;
+    }
+
+    // Add Last - O(1)
+    public void addLast(int data){
+        Node newNode = new Node(data);
+
+        if(tail == null){
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode ;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addLast(13);
+        ll.addLast(14);
     }
 }
