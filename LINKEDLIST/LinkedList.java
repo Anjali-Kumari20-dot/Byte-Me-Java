351e1021bd12a16bb712875d90c51436bc1e28de
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-07-27 15:18:09 +0530

    Created Liked list class and Head and tail node

---
+++ LINKEDLIST/LinkedList.java
@@ -0,0 +1,21 @@
+package LINKEDLIST;
+
+public class LinkedList {
+    public class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+    }
+}
