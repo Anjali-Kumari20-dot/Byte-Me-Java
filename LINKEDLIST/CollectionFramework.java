8cb14b9c3d58abca6f82eba33a039d8664d707e9
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-07-31 14:39:02 +0530

    Java Collection Frameworks

---
+++ LINKEDLIST/CollectionFramework.java
@@ -0,0 +1,15 @@
+package LINKEDLIST;
+
+import java.util.LinkedList;
+
+public class CollectionFramework {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        ll.addFirst(1);
+        ll.addLast(2);
+        ll.addLast(3);
+
+        System.out.println(ll);
+    }
+}
