9ddfab4179080692b3fa379266313a766c22bccc
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-08-02 15:45:52 +0530

    revision Of Singly ll PART-1

--- LINKEDLIST/Revision/SinglyLL.java
+++ LINKEDLIST/Revision/SinglyLL.java
@@ -5,45 +5,80 @@ public class SinglyLL {
         int data;
         Node next;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
-            this.next = next;
+            this.next = null;
         }
     }
 
-    static Node head;
-    static Node tail;
-    static int size;
+    public static Node head;
+    public static Node tail;
+    public static int size;
 
-    public void addFirst(int data){
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++ ;
+    }
+
+    public void addLast(int data){
         Node newNode = new Node(data);
 
         if(head == null){
-            newNode = head = tail;
+            head = tail = newNode ;
         }
-
-        newNode.next = head;
-        head = newNode;
+        tail.next = newNode;
+        tail = newNode;
+        size++ ;
     }
 
-    public void printLL(){
-        Node temp = head;
+    public void addMiddle(int data, int pos){
+        Node newNode = new Node(data);
 
-        if(head == null){
+        if(head == null || pos == 0){
+            addFirst(data);
             return;
         }
 
-        while(temp != null){
-            System.out.print(temp.data + " -> ");
-            temp = temp.next;
-        }
+        Node temp = head;
+        int i = 0;
 
-        System.out.print("null");
+        while(i < pos - 1 && temp.next != null){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        size++;
+    }
+
+    public void printLL() {
+        if (head == null) {
+            return;
+        } else {
+
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+
+            System.out.println("null");
+        }
     }
 
     public static void main(String[] args) {
         SinglyLL ll = new SinglyLL();
         ll.addFirst(1);
+        ll.addLast(2);
+        ll.addMiddle(3, 1);
         ll.printLL();
+        System.out.println("Size of ll : "+ size);
     }
 }
