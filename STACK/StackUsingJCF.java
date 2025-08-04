5b38cfc078b437b04dcf983195b713b60ca0cd69
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-08-04 17:17:23 +0530

    Reverse string and push at bottom

--- STACK/StackUsingJCF.java
+++ STACK/StackUsingJCF.java
@@ -1,17 +1,46 @@
 package STACK;
-
 import java.util.Stack;
 
 public class StackUsingJCF {
+    public static void pushAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++){
+            s.push(str.charAt(i));
+        }
+
+        StringBuilder result = new StringBuilder();
+        while(!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+
+        return result.toString();
+    }
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
+        String str = "Anjali";
         s.push(1);
         s.push(2);
         s.push(3);
+        pushAtBottom(s, 4);
+        String res = reverseString(str);
+        
+        System.out.println(res);
 
-        while(!s.isEmpty()){
-            System.out.println(s.peek());
-            s.pop();
-        }
+        // while(!s.isEmpty()){
+        //     System.out.println(s.peek());
+        //     s.pop();
+        // }
     }
 }
