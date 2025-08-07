73da342cb46facec68bd0dc04d816a0d0e8f90a9
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-08-07 11:50:59 +0530

    Valid Parenthesis

--- STACK/ValidParenthesis.java
+++ STACK/ValidParenthesis.java
@@ -3,20 +3,29 @@ package STACK;
 import java.util.Stack;
 
 public class ValidParenthesis {
-    public static Boolean isValid(String str){
-        Stack<Character> s= new Stack<>();
+    public static Boolean isValid(String str) {
+        Stack<Character> s = new Stack<>();
 
-        for(int i = 0; i < str.length(); i++){
+        for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
 
-            if(ch == '(' || ch == '{' || ch == '['){
+            if (ch == '(' || ch == '{' || ch == '[') {
                 s.push(ch);
-            }else if(ch == ')' || ch == '}' || ch == ']'){
-                s.pop();
+            } else {
+                if (s.empty())
+                    return false;
+
+                if ((s.peek() == '(' && ch == ')') || (s.peek() == '{' && ch == '}')
+                        || (s.peek() == '[' && ch == ']')) {
+                    s.pop();
+                } else {
+                    return false;
+                }
             }
         }
         return s.empty();
     }
+
     public static void main(String[] args) {
         String str1 = "({[]}()";
         String str2 = "{[]}()";
