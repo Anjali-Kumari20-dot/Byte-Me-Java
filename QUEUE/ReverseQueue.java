af85992cc8d440def2b3cb3fdc9d9b95c6f1b44b
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-08-17 15:32:26 +0530

    Reverse first k em=lements

--- QUEUE/ReverseQueue.java
+++ QUEUE/ReverseQueue.java
@@ -16,6 +16,28 @@ public class ReverseQueue {
             q.add(s.pop());
         }
     }
+    public static void reverseFirstK(Queue<Integer> q, int k){
+        int size = q.size();
+        Stack<Integer> s = new Stack<>();
+        // step 1: push in stack upto k elements
+        for(int i = 0; i < k; i++) {
+            s.push(q.remove());
+        }
+
+        // step 2: add to queue
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        }
+
+        // step 3: reverse the rest elements
+        for(int i = 0; i < size - k; i++){
+            q.add(q.remove());
+        }
+
+        while(!s.empty()){
+            q.add(s.pop());
+        }
+    }
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
         q.add(1);
@@ -23,7 +45,12 @@ public class ReverseQueue {
         q.add(3);
         q.add(4);
         q.add(5);
-        reverseQ(q);
+        // reverseQ(q);
+        // while(!q.isEmpty()){
+        //     System.out.print(q.peek() + " ");
+        //     q.remove();
+        // }
+        reverseFirstK(q, 3);
         while(!q.isEmpty()){
             System.out.print(q.peek() + " ");
             q.remove();
