88ecca9f1cf08084a6d73d1ee9dd4dfc3e4af7c8
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-08-28 09:19:21 +0530

    Post Order

---
+++ BINARY_TREES/BuildTreePostorder.java
@@ -0,0 +1,51 @@
+package BINARY_TREES;
+
+public class BuildTreePostorder {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]){
+            idx++;
+
+            if(nodes[idx] == -1){
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void postorder(Node root){ // O(n)
+        if(root == null){
+            System.out.print("-1 ");
+            return;
+        }
+        postorder(root.left); // left
+        postorder(root.right); // right
+        System.out.print(root.data + " "); // root
+    }
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        Node root = BinaryTree.buildTree(nodes);
+        System.out.println(root.data);
+ 
+        postorder(root);
+    }
+}
+
