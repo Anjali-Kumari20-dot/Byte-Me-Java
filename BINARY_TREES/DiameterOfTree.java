7568867afaa75e83e2a7cba9074588d1aba63a7a
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-08-30 11:29:15 +0530

    Optimised Diameter of a tree

--- BINARY_TREES/DiameterOfTree.java
+++ BINARY_TREES/DiameterOfTree.java
@@ -5,15 +5,16 @@ public class DiameterOfTree {
         int data;
         Node left, right;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
         }
     }
 
-    public static int height(Node root){
-        if(root == null) return 0;
+    public static int height(Node root) {
+        if (root == null)
+            return 0;
 
         int lh = height(root.left);
         int rh = height(root.right);
@@ -21,26 +22,52 @@ public class DiameterOfTree {
         return Math.max(lh, rh) + 1;
     }
 
-    public static int diameter(Node root){
-        if(root == null) return 0;
+    public static int diameterI(Node root) { // O(n^2)
+        if (root == null)
+            return 0;
 
-        int leftDiam = diameter(root.left);
+        int leftDiam = diameterI(root.left);
         int leftHt = height(root.left);
-        int rightDiam = diameter(root.right);
+        int rightDiam = diameterI(root.right);
         int rightHt = height(root.right);
 
         int selfDiam = leftHt + rightHt + 1;
 
         return Math.max(selfDiam, Math.max(leftDiam, rightDiam));
     }
+
+    static class Info {
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht) {
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    public static Info diameterII(Node root) { // O(n)
+        if(root == null){
+            return new Info(0, 0);
+        }
+
+        Info leftInfo = diameterII(root.left);
+        Info rightInfo = diameterII(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+        return new Info(diam, ht);
+    }
+
     public static void main(String[] args) {
         /*
-                    1 
-                   / \
-                 2     3
-                / \   / \
-               4   5 6   7
-
+         * 1
+         * / \
+         * 2 3
+         * / \ / \
+         * 4 5 6 7
+         * 
          */
         Node root = new Node(1);
         root.left = new Node(2);
@@ -50,7 +77,10 @@ public class DiameterOfTree {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        int diam = diameter(root);
+        int diam = diameterII(root).diam;
         System.out.println("Diameter of tree : " + diam);
+
+        int ht = diameterII(root).ht;
+        System.out.println("Height of tree : " + ht);
     }
 }
