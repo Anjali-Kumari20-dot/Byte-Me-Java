2a402e22b20519bc91ae5c355638d9c3d25b8fa2
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-09-20 18:41:06 +0530

    Validate a BST

--- BINARY_SEARCH_TREES/Main.java
+++ BINARY_SEARCH_TREES/Main.java
@@ -8,69 +8,68 @@ public class Main {
         Node right;
         int data;
 
-        Node(int data){
+        Node(int data) {
             this.left = null;
             this.right = null;
             this.data = data;
         }
-    } 
-     public static Node insert(Node root, int val){
-        if(root == null){
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
             root = new Node(val);
             return root;
         }
 
-        if(root.data > val){
+        if (root.data > val) {
             // left subtree
             root.left = insert(root.left, val);
-        }else {
+        } else {
             // right subtree
             root.right = insert(root.right, val);
         }
         return root;
     }
 
-    public static void inorder(Node root){
-        if(root == null){
+    public static void inorder(Node root) {
+        if (root == null) {
             return;
         }
         inorder(root.left);
-        System.out.print(root.data+ " ");
+        System.out.print(root.data + " ");
         inorder(root.right);
     }
 
-    public static boolean search(Node root, int key){ // TC - O(H)
-        if(root == null) {
+    public static boolean search(Node root, int key) { // TC - O(H)
+        if (root == null) {
             return false;
         }
-        if(root.data == key){
+        if (root.data == key) {
             return true;
         }
 
-        if(root.data > key){
+        if (root.data > key) {
             return search(root.left, key);
-        }else {
+        } else {
             return search(root.right, key);
         }
     }
 
-    public static Node delete(Node root, int val){
-        if(root.data < val){
+    public static Node delete(Node root, int val) {
+        if (root.data < val) {
             root.right = delete(root.right, val);
-        }
-        else if(root.data > val){
+        } else if (root.data > val) {
             root.left = delete(root.left, val);
-        }
-        else {
+        } else {
             // case 1 - leaf node
-            if(root.left == null && root.right == null){
+            if (root.left == null && root.right == null) {
                 return null;
             }
 
             // case 2 - single child
-            if(root.left == null){
+            if (root.left == null) {
                 return root.right;
-            }else if(root.right == null){
+            } else if (root.right == null) {
                 return root.left;
             }
 
@@ -79,47 +78,45 @@ public class Main {
             root.data = IS.data;
             root.right = delete(root.right, IS.data);
         }
-        return root; 
+        return root;
     }
 
-    public static Node findInorderSuccessor(Node root){
+    public static Node findInorderSuccessor(Node root) {
         while (root.left != null) {
             root = root.left;
         }
         return root;
     }
 
-    public static void printInRange(Node root, int k1, int k2){
-        if(root == null){
+    public static void printInRange(Node root, int k1, int k2) {
+        if (root == null) {
             return;
         }
-        if(root.data >= k1 && root.data <= k2){
+        if (root.data >= k1 && root.data <= k2) {
             printInRange(root.left, k1, k2);
             System.out.print(root.data + " ");
             printInRange(root.right, k1, k2);
-        }
-        else if (root.data < k1){
+        } else if (root.data < k1) {
             printInRange(root.left, k1, k2);
-        }
-        else{
+        } else {
             printInRange(root.right, k1, k2);
         }
     }
 
-    public static void printPath(ArrayList<Integer> path){
-        for(int i = 0; i < path.size(); i++){
+    public static void printPath(ArrayList<Integer> path) {
+        for (int i = 0; i < path.size(); i++) {
             System.out.print(path.get(i) + " -> ");
         }
         System.out.println("Null");
     }
 
-    public static void printRoot2Leaf(Node root, ArrayList<Integer> path){
-        if(root == null){
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path) {
+        if (root == null) {
             return;
         }
 
         path.add(root.data);
-        if(root.left == null && root.right == null){
+        if (root.left == null && root.right == null) {
             printPath(path);
         }
 
@@ -127,30 +124,54 @@ public class Main {
         printRoot2Leaf(root.right, path);
         path.remove(path.size() - 1);
     }
+
+    public static boolean isValidBST(Node root, Node min, Node max){
+        if(root == null){
+            return true;
+        }
+
+        if(min != null && root.data <= min.data){
+            return false;
+        }
+
+        else if(max != null && root.data >= max.data){
+            return false;
+        }
+
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+
+    }
+
     public static void main(String[] args) {
-        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
 
-        for(int i = 0; i < values.length; i++){
+        for (int i = 0; i < values.length; i++) {
             root = insert(root, values[i]);
         }
         inorder(root);
         System.out.println();
+
+        if(isValidBST(root, null, null)){
+            System.out.println("Valid BST");
+        }else{
+            System.out.println("Not valid BST");
+        }
 
         printRoot2Leaf(root, new ArrayList<>());
 
         printInRange(root, 5, 12);
         System.out.println();
 
-        if(search(root, 6)){
+        if (search(root, 6)) {
             System.out.print("found");
-        }else {
+        } else {
             System.out.println("Not found");
         }
 
         root = delete(root, 1);
         System.out.println();
-        
+
         inorder(root);
     }
 }
