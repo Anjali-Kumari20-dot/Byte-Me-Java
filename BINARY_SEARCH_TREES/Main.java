fad4a13f09f47fd272b5e4747cd10e1286fd3c96
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-09-20 17:47:24 +0530

    Print in a given range

--- BINARY_SEARCH_TREES/Main.java
+++ BINARY_SEARCH_TREES/Main.java
@@ -12,8 +12,7 @@ public class Main {
             this.data = data;
         }
     } 
-
-    public static Node insert(Node root, int val){
+     public static Node insert(Node root, int val){
         if(root == null){
             root = new Node(val);
             return root;
@@ -88,6 +87,23 @@ public class Main {
         return root;
     }
 
+    public static void printInRange(Node root, int k1, int k2){
+        if(root == null){
+            return;
+        }
+        if(root.data >= k1 && root.data <= k2){
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        }
+        else if (root.data < k1){
+            printInRange(root.left, k1, k2);
+        }
+        else{
+            printInRange(root.right, k1, k2);
+        }
+    }
+
     public static void main(String[] args) {
         int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
@@ -98,7 +114,10 @@ public class Main {
         inorder(root);
         System.out.println();
 
-        if(search(root, 9)){
+        printInRange(root, 5, 12);
+        System.out.println();
+
+        if(search(root, 6)){
             System.out.print("found");
         }else {
             System.out.println("Not found");
