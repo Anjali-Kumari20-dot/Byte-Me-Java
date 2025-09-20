8e3b32b512d92eedc8e37222458f50359b313881
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-09-20 18:18:48 +0530

    Print Root to Leaf

--- BINARY_SEARCH_TREES/Main.java
+++ BINARY_SEARCH_TREES/Main.java
@@ -1,5 +1,7 @@
 package BINARY_SEARCH_TREES;
 
+import java.util.ArrayList;
+
 public class Main {
     static class Node {
         Node left;
@@ -104,6 +106,27 @@ public class Main {
         }
     }
 
+    public static void printPath(ArrayList<Integer> path){
+        for(int i = 0; i < path.size(); i++){
+            System.out.print(path.get(i) + " -> ");
+        }
+        System.out.println("Null");
+    }
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path){
+        if(root == null){
+            return;
+        }
+
+        path.add(root.data);
+        if(root.left == null && root.right == null){
+            printPath(path);
+        }
+
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size() - 1);
+    }
     public static void main(String[] args) {
         int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
@@ -113,6 +136,8 @@ public class Main {
         }
         inorder(root);
         System.out.println();
+
+        printRoot2Leaf(root, new ArrayList<>());
 
         printInRange(root, 5, 12);
         System.out.println();
