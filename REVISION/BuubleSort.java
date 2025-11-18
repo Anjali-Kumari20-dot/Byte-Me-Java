7217b60508abf6c526d3861ed6c81ea9f3b583bf
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-11-18 18:35:09 +0530

    Bubble sort updated

--- REVISION/BuubleSort.java
+++ REVISION/BuubleSort.java
@@ -5,20 +5,24 @@ public class BuubleSort {
         // int arr[] = {5, 1, 4, 2, 8};
         int arr[] = {-2, -4, -6, -1, -3};
         int n = arr.length;
+        boolean swapped = false;
 
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                if(arr[i] > arr[j]){
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j < n - i - 1; j++){
+                if(arr[j] > arr[j + 1]){
                     // swap
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
                 }
             }
+            if(!swapped)  break;
         }
         System.out.println("Sorted array:");
         for(int i = 0; i < n; i++){
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + ", ");
         }
     }
 }
